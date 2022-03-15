83301fa3dc5e502e3f3d69227209fadc102806e8
Coin Monster <coinmonster999@gmail.com>
2022-03-15 18:26:46 +0100

    Update Header.tsx

--- components/Homepage/Header.tsx
+++ components/Homepage/Header.tsx
@@ -19,7 +19,7 @@ const Header: NextComponentType = () => {
     <div className={styles.header} id="home">
         <div className={styles.headerContainer}>
             <img loading="lazy" className={styles.mainLogo} src="/media/kaiju_Full.gif" />
-            <h1 className={styles.landingText}>Kaiju Kongz is a 3333 NFT collection with a real-time burn phase. It willl become the first collection to consiste of entirely 1/1s Earn $HUMAN tokens for a ton of utilities from P2E, WL purchase, VX minting, partner project integration and many more</h1>
+            <h1 className={styles.landingText}>Kaiju Kongz is a 3333 NFT collection with a real-time burn phase. It willl become the first collection to consist of entirely 1/1s Earn $HUMAN tokens for a ton of utilities from P2E, WL purchase, VX minting, partner project integration and many more</h1>
             
             <a className={styles.buyBtn} href="#" target="_blank" rel="noreferrer">COMING SOON</a>
         </div>
